159f72fe5a4306b4b8053c15b8d22617ab37ebb2
vuongps38770 <166083538+vuongps38770@users.noreply.github.com>
2025-11-13 20:54:46 +0700

    completed add_2_nums

--- add_2_nums/add_2_nums.py
+++ add_2_nums/add_2_nums.py
@@ -1,1 +1,12 @@
-## todo: mai làm nay hơi lười
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num>=10:
+            su = 0
+            while num>0:
+                su += num % 10
+                num//=10
+            num = su
+        return num
+sol = Solution()
+test = 36
+print(sol.addDigits(test))
